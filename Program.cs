c662f4ec295ebc93191c4d8b7bf325fa06432639
kero-edward <kyrillos.edward17@gmail.com>
2024-07-08 22:11:59 +0300

    Create an array of Employees with size three a DBA, Guest and the third one is security officer who have full permissions. (Employee [] EmpArr;)

--- Program.cs
+++ Program.cs
@@ -6,10 +6,27 @@ namespace oop_assignment2
     {
         static void Main(string[] args)
         {
-            HiringDate hireDate = new HiringDate(15, 7, 2021);
-            Employee emp = new Employee(1, "John Doe", SecurityLevel.Developer, 75000m, hireDate, Gender.M);
+            //HiringDate hireDate = new HiringDate(15, 7, 2021);
+            //Employee emp = new Employee(1, "John Doe", SecurityLevel.Developer, 75000m, hireDate, Gender.M);
 
-            Console.WriteLine(emp);
+            //Console.WriteLine(emp);
+
+
+
+            HiringDate hireDate1 = new HiringDate(10, 5, 2020);
+            HiringDate hireDate2 = new HiringDate(3, 9, 2021);
+            HiringDate hireDate3 = new HiringDate(15, 6, 2019);
+
+            Employee[] EmpArr = new Employee[3];
+
+            EmpArr[0] = new Employee(1, "DBA Employee", SecurityLevel.DBA, 85000m, hireDate1, Gender.M);
+            EmpArr[1] = new Employee(2, "Guest Employee", SecurityLevel.Guest, 50000m, hireDate2, Gender.F);
+            EmpArr[2] = new Employee(3, "Security Officer", SecurityLevel.Developer, 100000m, hireDate3, Gender.M);
+
+            foreach (var emp in EmpArr)
+            {
+                Console.WriteLine(emp);
+            }
         }
     }
 }
