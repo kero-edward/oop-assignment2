c75752504f8e2352452e2c6f71c3c8575e5627c9
kero-edward <kyrillos.edward17@gmail.com>
2024-07-08 21:32:23 +0300

    Designning and implementing a Class for the employees in a company. Employee is identified by an ID, Name, security level, salary, hire date and Gender.

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_assignment2
+{
+    internal class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SecurityLevel { get; set; }
+        public decimal Salary { get; set; }
+        public string HireDate { get; set; }
+        public string Gender { get; set; }
+    }
+}
